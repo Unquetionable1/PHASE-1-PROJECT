61dbeb1e0e95e138b1801f0adc901c98a1d49ce8
Edmound <eddnjoori@gmail.com>
2024-04-15 09:14:00 +0300

    Creats html ,css and js files

---
+++ src/index.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    getImage()
+    const breed = document.getElementById('breed');
+    breed.addEventListener('click',()=>{
+     getBreed()
+    })
+
+    
+})
+function getImage(){
+    fetch('https://api.thecatapi.com/v1/images/search?limit=1')
+    .then(res=>{
+        if(!res.ok){
+            new Error('You have a request error')
+        }
+        return res.json();
+    })
+    .then(imagData=>{
+        imagData.map(image=>{
+            const div = document.getElementById('view');
+            const img=document.createElement('img');
+            img.src=`${image.url}`;
+            img.height="300"
+            img.width='300'
+            div.appendChild(img);
+        })
+    })
+    .catch(error=>console.log(error))
+}
+
+function getBreed(){
+    fetch('https://api.thecatapi.com/v1/breeds?limit=10&page=0')
+    .then(res=>{
+        if(!res.ok){
+            new Error('You have a request error')
+        }
+        return res.json();
+    })
+    .then(breed => {
+        breed.forEach(breeds => {
+            let ul = document.createElement('ul');
+            let div = document.getElementById('list')
+            ul.innerHTML +=`<li>Name: ${breeds.name}.<hr> Description: ${breeds.description}.
+            </li>.<button id='img>View image</button>`
+            div.appendChild(ul);
+
+        });
+    })
+
+}
+
+
